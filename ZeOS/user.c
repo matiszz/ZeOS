6271e2aaaa720f560b63e9fd45ff237c70fb1da1
matiszz <matias.szbar@gmail.com>
2017-11-02 12:23:12 +0100

    Ultimos detalles

--- ZeOS/user.c
+++ ZeOS/user.c
@@ -6,26 +6,18 @@ int pid;
 int __attribute__ ((__section__(".text.main")))
 
 main(void) {
-  write(1, " \n hola mati. PID: ", strlen(" \n hola mati. PID: "));
-  int pid = getpid();
-  char buf[10];
-  itoa(pid, buf);
-  write(1, buf, strlen(buf));
 
-  int pid2 = fork();
-  if (pid2 == 0) {
-    write(1, "\n hola soy el hijo ", strlen("\n hola soy el hijo "));
-    itoa(getpid(), buf);
-    write(1, buf, strlen(buf));
+  int p = fork();
+  if (p == 0) {
+    write(1, "\n  Hola, soy el hijo, mi PID es ", strlen("\n  Hola, soy el hijo, mi PID es "));
+    itoa(getpid(), buff);
+    write(1, buff, strlen(buff));
   }
-  else if (pid2 > 0) {
-    write(1, "\n hola soy el padre ", strlen("\n hola soy el padre "));
-    itoa(getpid(), buf);
-    write(1, buf, strlen(buf));
-    while(1);
+  else if (p > 0) {
+    write(1, "\n  Hola, soy el padre, mi PID es ", strlen("\n  Hola, soy el padre, mi PID es "));
+    itoa(getpid(), buff);
+    write(1, buff, strlen(buff));
   }
-  else if (pid2 < 0) write(1, "no funca man", strlen("no funca man"));
-  else write(1, "uy aqui si que no funca", strlen("uy aqui si que no funca"));
 
   while(1) { }
 }
